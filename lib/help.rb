1f32459245290901367da6ee284ee500c4bf4fb6
Dan Bickford <danbickford007@yahoo.com>
2014-02-24 22:13:05 -0500

    help menu

--- lib/help.rb
+++ lib/help.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Help
 
   def initialize
@@ -5,20 +7,26 @@ class Help
   end
 
   def info
-    print "Usage: \n"
-    print "_________________________________________________________\n"
-    print "regex \n"
-    print "\tset or add myRegex                  -sets your current regex\n"
-    print "\tview                         -view current regex\n"
+    print "Usage: \n".red
+    print "_________________________________________________________\n".red
+    print "regex \n".yellow
+    print "\tset or add myRegex           -sets your current regex\n".blue
+    print "\tview                         -view current regex\n".blue
+    print "\t**only add regex between but not including the starting and ending //\n\n".blue
 
-    print "strings\n"
-    print "\tadd myString                 -add test string to your list of strings\n"
-    print "\tremove myString              -remove test string from list of strings\n"
-    print "\tview or show or list         -view your strings\n"
-    print "\tclear_all                    -remove all your test strings\n"
+    print "strings\n".yellow
+    print "\tadd myString                 -add test string to your list of strings\n".blue
+    print "\tremove myString              -remove test string from list of strings\n".blue
+    print "\tview or show or list         -view your strings\n".blue
+    print "\tclear_all                    -remove all your test strings\n".blue
 
-    print "check\n"
-    print "\tnow                          -run your regex against all test strings"
+    print "\tcheck now                      -run your regex against all test strings\n\n".green
+    
+    
+    print "***EXAMPLES***\n".yellow
+    print "\tset regex:         regex set \d\n".blue
+    print "\tadd test string:   strings add testString\n".blue
+    print "\trun your regex:    check now\n".blue
   end
 
 end
